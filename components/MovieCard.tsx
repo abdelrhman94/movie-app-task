bb707393da487fe70e4e06804e839a74dfe070a2
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-06-03 22:06:19 +0300

    enhancement images

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -14,17 +14,20 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
               : `${process.env.EMPTY_MOVIE_URL}`
           }
           alt={movie?.title}
-          fill={true}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority={true}
+          placeholder="empty"
         />
       </div>
       <div className="flex gap-4 justify-center items-center mt-3 bg-red">
         <h2 className="text-lg font-medium">{movie?.title}</h2>
         <span
           className={`flex flex-col p-1.5 text-white rounded-full ${
-             movie?.vote_average <= 5 ? `bg-red-700` : `bg-green-700`
+            movie?.vote_average <= 5 ? `bg-red-700` : `bg-green-700`
           }`}
         >
-          {movie?.vote_average}
+          {movie?.vote_average.toFixed(1)}
         </span>
       </div>
     </Link>
