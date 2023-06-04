5de170dbdf9583282d1d7cc6a42c476f36673bb5
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-06-04 17:43:18 +0300

    implement upcoming and toprated movies components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import MoviesServices from '@/services/MoviesServices';
 import PopularMovies from '@/components/PopularMovies';
+import UpcomingMovies from '@/components/UpcomingMovies';
+import TopRatedMovies from '@/components/TopRatedMovies';
 
 export default async function Home() {
   const popularMovies = await MoviesServices.getMoviesByType('popular');
+  const upcomingMovies = await MoviesServices.getMoviesByType('upcoming');
+  const topRatedMovies = await MoviesServices.getMoviesByType('top_rated');
 
   return (
     <main className="mt-5 flex flex-col flex-wrap">
       <div className="w-[1300px] max-w-full px-4 mx-auto">
         <PopularMovies popularMovies={popularMovies} />
+        <UpcomingMovies upcomingMovies={upcomingMovies} />
+        <TopRatedMovies topRatedMovies={topRatedMovies} />
       </div>
     </main>
   );
---
+++ components/TopRatedMovies.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { Movie } from '@/types/movies';
+import MovieCard from './MovieCard';
+
+const TopRatedMovies = ({ topRatedMovies }: { topRatedMovies: any }) => {
+  return (
+    <div className="flex flex-col mb-6">
+      <div className="flex justify-between items-center mt-4">
+        <h1 className="text-2xl font-medium">Top Rated Movies</h1>
+        <Link
+          href="/movies/top-rated"
+          className="py-2 px-5 bg-slate-800 text-md font-normal text-white"
+        >
+          See all
+        </Link>
+      </div>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 mt-4 gap-4">
+        {topRatedMovies.results.slice(0, 4).map((movie: Movie) => (
+          <MovieCard key={movie?.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopRatedMovies;
